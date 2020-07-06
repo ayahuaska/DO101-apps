5a2e36a5329935afbfce349e053479737cd9599f
ayahuaska <srrraul@yahoo.es>
2020-07-06 13:50:51 +0400

    añadida la parte marciana /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello Worldsss del mundo mundial!\n');
+});
+app.get('/mars', function (req, res) {
+  res.send('Hello Marcianicos de Marte!\n');
 });
 
 app.listen(8080, function () {

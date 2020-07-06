4b52c20cbffdf5ad8a3eb0f4a910555ddfb64007
ayahuaska <srrraul@yahoo.es>
2020-07-06 14:03:54 +0400

    añadido el /venus

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -7,6 +7,9 @@ app.get('/', function (req, res) {
 app.get('/mars', function (req, res) {
   res.send('Hello Marcianicos de Marte!\n');
 });
+app.get('/venus', function (req, res) {
+  res.send('saludos a mi gata de Venus!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
